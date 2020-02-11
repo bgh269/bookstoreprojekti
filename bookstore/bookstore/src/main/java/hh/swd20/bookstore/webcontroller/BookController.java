6cffc68fb2d6f95ea3f170212b7c75b2b572db57
bgh269 <ovaskainenkaisa@gmail.com>
2020-02-11 20:43:38 +0200

    ekat lisäykset

---
+++ bookstore/bookstore/src/main/java/hh/swd20/bookstore/webcontroller/BookController.java
@@ -0,0 +1,15 @@
+package hh.swd20.bookstore.webcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookController {
+
+	@RequestMapping(value="/index", method=RequestMethod.GET)
+	public String getBook(Model model) {
+		return "index"; //index.html
+	}
+}
